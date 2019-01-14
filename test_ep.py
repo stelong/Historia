6c72039303882493873ae0277fc447550d66b47f
stelong <stefano.longobardi.8@gmail.com>
2019-01-14 17:04:27 +0000

    test new stuff

--- test_ep.py
+++ test_ep.py
@@ -2,6 +2,8 @@ from ep import solver as s
 from ep.models import Gattoni_SHAM6Hz as sham
 from ep.models import Gattoni_AB6Hz as ab
 from utils import ep_out as e
+import numpy as np
+import matplotlib.pyplot as plt
 
 def main():
 
@@ -11,13 +13,17 @@ def main():
 	S.run2sc(p)
 	S.plot_calcium(scene='show')
 
-	# C = e.get_cabiomarkers(sol.t, sol.ca)
-	# ts = C.ts
-	# cas = C.cas
-	# a = C.a
-	# bio = C.bio
-	# s.plot_calcium(sol, scene='show')
-	# e.visualize_biomarkers(ts, a, bio, scene='show')
+	C = e.TruncatedCalcium(S.t, S.ca)
+	# C.get_cabiomarkers()
+	C.visualize_biomarkers(scene='show')
+
+	
+
+	# aa = np.copy(C.a1)
+	# aa[0] = 0.5*aa[0]
+	# plt.plot(C.t,e.F(C.t, *aa))
+	# plt.plot(C.t,e.F(C.t, *C.a1))
+	# plt.show()
 
 #-------------------------
 
