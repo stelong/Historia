bcfdbe154f4d65f6f5bd3adcff0fc7c6100b614b
stelong <stefano.longobardi.8@gmail.com>
2019-01-14 15:36:09 +0000

    EP class

--- ep/solver.py
+++ ep/solver.py
@@ -2,46 +2,48 @@ import numpy as np
 from scipy.integrate import solve_ivp
 import matplotlib.pyplot as plt
 
-class Solution():
-	def __init__(self, conv, t, ca):
-		self.conv = conv
-		self.t = t
-		self.ca = ca
+class EPSolution:
+	def __init__(self, model):
+		self.model = model
+		self.conv = 0
+		self.t = np.arange(0, 171, 1)
+		self.ca = np.zeros((171,), dtype=float)
 
-def solve_electr2ss(model):
-	p = model.initParams()
-	c = model.initConsts(p)
-	Y0 = model.initStates()
-	tspan = [0, 170]
-	t = range(0, 171)
+	def run2sc(self, parameters):
+		c = self.model.initConsts(parameters)
+		Y0 = self.model.initStates()
+		tspan = [0, 170]
 
-	Y = solve_ivp(fun=lambda t, y: model.odesys(t, y, c, p), t_span=tspan, y0=Y0, t_eval=t)
+		Y = solve_ivp(fun=lambda t, y: self.model.odesys(t, y, c, parameters), t_span=tspan, y0=Y0, t_eval=self.t)
+		self.ca = 1e3*Y.y[12, :]
+		self.conv = 1
 
-	tre = 10000
-	ssnc = b = 1
+	def run2ss(self, parameters):
+		c = self.model.initConsts(parameters)
+		Y0 = self.model.initStates()
+		tspan = [0, 170]
+		Y = solve_ivp(fun=lambda t, y: self.model.odesys(t, y, c, parameters), t_span=tspan, y0=Y0, t_eval=self.t)
 
-	while np.linalg.norm(Y.y[:, -1] - Y.y[:, 0], ord=np.inf) > 1e-1:
-		Y = solve_ivp(fun=lambda t, y: model.odesys(t, y, c, p), t_span=tspan, y0=Y.y[:, -1], t_eval=t)
-		ssnc += 1
+		thre = 10000
+		ssnc = b = 1
+		while np.linalg.norm(Y.y[:, -1] - Y.y[:, 0], ord=np.inf) > 1e-4:
+			Y = solve_ivp(fun=lambda t, y: self.model.odesys(t, y, c, parameters), t_span=tspan, y0=Y.y[:, -1], t_eval=self.t)
+			ssnc += 1
+			if ssnc == thre:
+				b = 0
+				break
 
-		if ssnc == tre:
-			b = 0
-			break
+		if b:
+			print('\n=== Number of cycles needed to reach the steady-state: {}\n'.format(ssnc))
+			self.ca = 1e3*Y.y[12, :]
+			self.conv = b
+		else:
+			print('\n=== Error: unable to reach the steady-state!\n')
 
-	if b:
-		print('\n=== Number of cycles needed to reach the steady-state: {}\n'.format(ssnc))
-	else:
-		print('\n=== Error: unable to reach the steady-state!\n')
-
-	return Solution(b, Y.t, 1e3*Y.y[12, :])
-
-def plot_calcium(sol, scene='do_not_show'):
-	plt.plot(sol.t, sol.ca)
-
-	if scene == 'show':
-		plt.xlim(sol.t[0], sol.t[-1])
-		plt.xlabel('Time [ms]')
-		plt.ylabel('Intracellular calcium [$\mu$M]')
-		plt.show()
-
-	return
+	def plot_calcium(self, scene='do_not_show'):
+		plt.plot(self.t, self.ca)
+		if scene == 'show':
+			plt.xlim(self.t[0], self.t[-1])
+			plt.xlabel('Time [ms]')
+			plt.ylabel('Intracellular calcium [$\mu$M]')
+			plt.show()
