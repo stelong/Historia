1b52fe90b02466084cde054093bb9fc568a5b7a7
stelong <stefano.longobardi.8@gmail.com>
2019-01-15 13:16:14 +0000

    phen calcium

--- utils/ep_out.py
+++ utils/ep_out.py
@@ -3,61 +3,64 @@ from scipy import optimize
 from scipy.optimize import curve_fit, brentq
 import matplotlib.pyplot as plt
 
-class TruncCalcium:
+class PhenCalcium:
 	def __init__(self, t, ca):
+		self.conv = 1
 		self.t = t[4:-1] - 4
 		self.ca = ca[4:-1]
 		self.a1 = []
+		self.a = []
 		self.bio = []
 
-	def get_cabiomarkers(self):
+	def fit(self):
 		a0 = A_output(self.ca)
 		a0[2] = np.log(2)/a0[2]
 
 		self.a1 = curve_fit(f, shift_for_a_sec(self.t), self.ca, p0=a0)[0]
+		self.a = np.copy(self.a1)
 
-		DCa = self.a1[0]
-		TP = brentq(df1, self.t[1], self.t[-1], args=self.a1)
-		PCa = f(TP, *self.a1)
-
+	def get_biomarkers(self):
+		DCa = self.a[0]
 		try:
-			RT50 = brentq(df2, TP, self.t[-1], args=(self.a1, TP)) - TP
+			TP = brentq(df1, self.t[1], self.t[-1], args=self.a)
+		except:
+			TP = -1
+		if TP != -1:
+			PCa = f(TP, *self.a)
+		else:
+			PCa = -1
+		try:
+			RT50 = brentq(df2, TP, self.t[-1], args=(self.a, TP)) - TP
 		except:
 			RT50 = -1
+		self.bio = np.array([DCa, PCa, RT50, TP])
 
-		self.bio = [DCa, PCa, RT50, TP]
+		if self.bio[3] == -1 or self.bio[2] == -1:
+			self.conv = 0
+		elif self.bio[0] < 0 or self.bio[1] < 0 or self.bio[2] < 0 or self.bio[3] < 0:
+			self.conv = 0
+		elif self.bio[0] >= self.bio[1]:
+			self.conv = 0
 
-	def visualize_biomarkers(self, scene='do_not_show'):
-		if len(self.a1) == 0 and len(self.bio) == 0:
-			print('Error: first get calcium biomarkers!')
-		else:
-			plt.plot(self.t, f(self.t, *self.a1))
+	def build_ca(self):
+		if self.conv:
+			self.ca = f(self.t, *self.a)
+
+	def plot(self, visbio=False, scene='do_not_show'):
+		plt.plot(self.t, f(self.t, *self.a))
+		if visbio:
 			plt.scatter(self.bio[3], self.bio[1], c='r')
-			plt.scatter(self.bio[2]+self.bio[3], f(self.bio[2]+self.bio[3], *self.a1), c='r')
+			plt.scatter(self.bio[2]+self.bio[3], f(self.bio[2]+self.bio[3], *self.a), c='r')
 			plt.axvline(x=self.bio[3], c='r', linestyle='dashed')
 			plt.axvline(x=self.bio[2]+self.bio[3], c='r', linestyle='dashed')
 			plt.axhline(y=self.bio[0], c='r', linestyle='dashed')
 			plt.axhline(y=self.bio[1], c='r', linestyle='dashed')
-			if scene == 'show':
-				plt.xlim(self.t[0], self.t[-1])
-				plt.xlabel('Time [ms]')
-				plt.ylabel('Truncated Calcium + Biomarkers')
-				plt.show()
-
-class PhenCalcium:
-	def __init__(self, t, a):
-		self.t = t
-		self.ca = np.zeros((167,), dtype=float)
-		self.a = a
-
-	def F(self):
-		if check_ca(self.a):
-			self.ca = 
-
+		if scene == 'show':
+			plt.xlim(self.t[0], self.t[-1])
+			plt.xlabel('Time [ms]')
+			plt.ylabel('Intracellular calcium [$\mu$M]')
+			plt.show()
 	
-def shift_for_a_sec(ts):
-	return [x + 1e-16 for x in ts]
-
 def A_output(ca):
 	N = len(ca)
 	t = np.linspace(0, N-1, N)
@@ -74,6 +77,9 @@ def A_output(ca):
 
 	return [DCa, PCa, RT50, TP]
 
+def shift_for_a_sec(ts):
+	return [x + 1e-16 for x in ts]
+
 def f(t, *a):
 	return (a[1] - a[0]) * np.power(np.power(t/a[3], -1.0) + np.exp(a[2]*(t - a[3])), -1.0) + a[0]
 
@@ -82,15 +88,3 @@ def df1(t, a):
 
 def df2(t, a, b):
 	return f(t, *a) - 0.5*(f(b, *a) + f(0, *a))
-
-def check_ca(a):
-	if a[2] == -1:
-		conv = 0
-	elif a[0] < 0 or a[0] < 0 or a[0] < 0 or a[0] < 0:
-		conv = 0
-	elif a[0] >= a[1]:
-		conv = 0
-	else:
-		conv = 1
-
-	return conv
