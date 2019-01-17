1d7358555afb024636456cbfd186abc2d818a66c
stelong <stefano.longobardi.8@gmail.com>
2019-01-17 17:05:38 +0000

    tutto

---
+++ classifier/svm.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn import metrics
+from sklearn import pipeline
+from sklearn import preprocessing
+from sklearn import svm
+from sklearn import model_selection
+from scipy.stats import uniform
+
+class SVMCla:
+	def __init__(self):
+		self.X = []
+		self.y = []
+		self.scaler = []
+		self.cla = []
+
+	def fit(self, X, y):
+		self.X = X
+		self.y = y
+
+		pipe = pipeline.Pipeline([
+			('scaler', preprocessing.StandardScaler()),
+			('cla', svm.SVC(cache_size=1000, class_weight='balanced'))
+		])
+		param_grid = [{'cla__kernel': ['linear'], 'cla__C': [1, 10, 100, 1000]},
+		{'cla__kernel': ['poly'], 'cla__C': [1, 10, 100, 1000], 'cla__degree': [2, 3, 4, 5], 'cla__gamma': [1e-1, 1e-2, 1e-3, 1e-4]},
+		{'cla__kernel': ['rbf'], 'cla__C': [1, 10, 100, 1000], 'cla__gamma': [1e-1, 1e-2, 1e-3, 1e-4]}]
+
+		gs = model_selection.GridSearchCV(pipe, param_grid, scoring='accuracy', n_jobs=-1,
+			cv=5, error_score=0, return_train_score=False)
+		gs.fit(self.X, self.y)
+
+		best_model = gs.best_estimator_
+		self.scaler = best_model.steps[0][1]
+		self.cla = best_model.steps[1][1]
+
+	def predict(self, X_new):
+		return self.cla.predict(self.scaler.transform(X_new))
+
+	def accuracy(self, X_test, y_test):
+		return self.cla.score(self.scaler.transform(X_test), y_test)
+
+	def hlc_sample(self, n_points):
+		in_dim = self.X.shape[1]
+	
+		p_min = np.zeros(shape=(in_dim,), dtype=float)
+		p_max = np.zeros(shape=(in_dim,), dtype=float)
+		for i in range(in_dim):
+			p_min[i] = np.min(self.X[:, i]) 
+			p_max[i] = np.max(self.X[:, i])
+
+		np.random.seed()
+		
+		D = np.zeros(shape=(1, in_dim), dtype=float)
+		while D.shape[0] - 1 < n_points:
+			zu = uniform.rvs(loc=0, scale=1, size=in_dim)
+			d = np.zeros(shape=(1, in_dim), dtype=float)
+
+			for i in range(in_dim):
+				d[0, i] = p_min[i] + (p_max[i] - p_min[i])*zu[i]
+
+			pred = self.predict(d)
+			if pred:
+				D = np.vstack((D, d))
+
+		return D[1:, :]
+
+	def plot_accuracy_demo(self, X_test, y_test):
+		score = self.accuracy(X_test, y_test)
+		y_pred = self.predict(X_test)
+		cm = metrics.confusion_matrix(y_test, y_pred)
+		
+		plt.figure(figsize=(9,9))
+		sns.heatmap(cm, annot=True, fmt=".3f", linewidths=.5, square = True, cmap = 'Blues_r');
+		plt.ylabel('Actual label');
+		plt.xlabel('Predicted label');
+		all_sample_title = 'Accuracy score: {0}'.format(score)
+		plt.title(all_sample_title, size = 15);
+		plt.show()
---
+++ test_cla.py
@@ -0,0 +1,34 @@
+from ep import solver as s
+from ep.models import Gattoni_SHAM6Hz as sham
+from ep.models import Gattoni_AB6Hz as ab
+from utils import ep_out as e
+from classifier import svm as c
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+
+def main():
+
+	clf = c.SVMCla()
+
+	inFile1 = 'data/inputs_12.txt'
+	inFile2 = 'data/outputs_2.txt'
+	X = np.loadtxt(inFile1, dtype=float)
+	y = np.loadtxt(inFile2, dtype=int)
+
+	clf.fit(X, y)
+
+	# D = clf.hlc_sample(100)
+
+	# print(D.shape)
+	# print(D)
+
+	# print(clf.predict(D))
+
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+	clf.plot_accuracy_demo(X_test, y_test)
+
+#-------------------------
+
+if __name__ == "__main__":
+    main()
