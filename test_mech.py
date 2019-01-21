aab5907a365c85c89f9ab67da2b829c0fca3cd5f
stelong <stefano.longobardi.8@gmail.com>
2019-01-21 11:24:24 +0000

    with functions is better

--- test_mech.py
+++ test_mech.py
@@ -3,70 +3,64 @@ from mech import scan_logfile as slf
 from utils import math_tools as mt
 from utils import mech_out as glv
 
-def main():
-
-	# for j in range(50):
-	# 	tag = '/home/sl18/Desktop/AB_7/options1/' + str(j+1) + '/output_log.txt'
-	# 	M = slf.MECHSolution(tag)
-	# 	M.extract_loginfo()
-
-	# 	V = glv.LeftVentricle()
-	# 	V.get_lvfeatures(M, 2)
-		
-	# 	if V.conv:
-	# 		V.plot(M)
-
-	# ------------------------------------------------------------------------------
-
-	file_in = 'AB_9_in.txt'
-	with open(file_in, 'w') as f1:
+def looknsave_all(name):
+	folder = 'data/' + name 
+	file_name = name + '_inputs.txt'
+	vc = []
+	with open(file_name, 'w') as f:
 		for k in range(4):
-			s = 'data/AB_9/options' + str(k+1) + '/'
-			P = np.zeros(shape=(1, 8), dtype=float)
-		
+			s = folder + '/options' + str(k+1) + '/'
+			M = np.zeros(shape=(1, 8), dtype=float)
+			for j in range(200):
+				tag = s + str(j+1) + '/output_log.txt'
+				S = slf.MECHSolution(tag)
+				S.extract_loginfo()
+				M = np.vstack((M, np.asarray(S.p)))
+				V = glv.LeftVentricle()
+				V.get_lvfeatures(S, 2)
+				vc.append(V.conv)
+			np.savetxt(f, M[1:, :], fmt='%f')
+	f.close()
+
+	file_name = name + '_outputs.txt'
+	with open(file_name, 'w') as f:
+		np.savetxt(f, vc, fmt='%d')
+	f.close()
+	return
+
+def looknsave_conly(name, suffix):
+	folder = 'data/' + name 
+	file_name = name + '_conly_' + suffix + '.txt'
+
+	with open(file_name, 'w') as f:
+		for k in range(4):
+			s = folder + '/options' + str(k+1) + '/'
+			if suffix == 'inputs':
+				dim = 8
+			else:
+				dim = 11
+			M = np.zeros(shape=(1, dim), dtype=float)
 			for j in range(200):
 				tag = s + str(j+1) + '/output_log.txt'
 				S = slf.MECHSolution(tag)
 				S.extract_loginfo()
 				V = glv.LeftVentricle()
 				V.get_lvfeatures(S, 2)
-				# if V.conv:
-				P = np.vstack((P, np.asarray(S.p)))
-			np.savetxt(f1, P[1:, :], fmt='%f')
-	f1.close()
+				if V.conv:
+					if suffix == 'inputs':
+						M = np.vstack((M, np.asarray(S.p)))
+					else:
+						M = np.vstack((M, np.asarray(V.f)))
+			np.savetxt(f, M[1:, :], fmt='%f')
+	f.close()
+	return
 
-	# vconv = []
-	# for k in range(4):
-	# 	s = 'data/AB_9/options' + str(k+1) + '/'
-	# 	for j in range(200):
-	# 		tag = s + str(j+1) + '/output_log.txt'
-	# 		S = slf.MECHSolution(tag)
-	# 		S.extract_loginfo()	
-	# 		V = glv.LeftVentricle()
-	# 		V.get_lvfeatures(S, 2)	
-	# 		vconv.append(V.conv)
+def main():
+	name = 'AB_11'
 
-	# file = 'AB_9_conv.txt'
-	# with open(file, 'w') as f:
-	# 	np.savetxt(f, vconv, fmt='%d')
-	# f.close()		
-	
-	# file_out = 'AB_9_out.txt'
-	# with open(file_out, 'w') as f2:
-	# 	for k in range(4):
-	# 		s = 'data/AB_9/options' + str(k+1) + '/'
-	# 		M = np.zeros(shape=(1, 11), dtype=float)
-
-	# 		for j in range(200):
-	# 			tag = s + str(j+1) + '/output_log.txt'
-	# 			S = slf.MECHSolution(tag)
-	# 			S.extract_loginfo()
-	# 			V = glv.LeftVentricle()
-	# 			V.get_lvfeatures(S, 2)
-	# 			if V.conv:
-	# 				M = np.vstack((M, np.asarray(V.f)))
-	# 		np.savetxt(f2, M[1:, :], fmt='%f')
-	# f2.close()
+	looknsave_conly(name, 'inputs')
+	looknsave_conly(name, 'outputs')
+	looknsave_all(name)
 
 # ------------------------
 
