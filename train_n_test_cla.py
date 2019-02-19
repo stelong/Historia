33bbef897aa49c7ec533053214a4e5b7be1acf14
stelong <stefano.longobardi.8@gmail.com>
2019-02-19 11:50:53 +0000

    new routines

--- train_n_test_cla.py
+++ train_n_test_cla.py
@@ -1,34 +1,31 @@
+from classifier import svm
 import numpy as np
 import pickle
-from classifier import svm
-from sklearn.model_selection import train_test_split
 
 def main():
-
 	cla = svm.SVMCla()
 
-	inFile1 = 'data/ep/sham/inputs_all.txt'
-	inFile2 = 'data/ep/sham/outputs_all.txt'
+	inFile1 = 'data/SHAM_20/SHAM_20_inputs.txt'
+	inFile2 = 'data/SHAM_20/SHAM_20_outputs.txt'
+	inFile3 = 'data/SHAM_20/a1.txt'
 	
-	X = np.loadtxt(inFile1, dtype=float)
-	y = np.loadtxt(inFile2, dtype=float)
+	mech = np.loadtxt(inFile1, dtype=float)
+	a1 = np.loadtxt(inFile3, dtype=float)
 
-	# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=None)
+	X = np.hstack((a1, mech))
+	y = np.loadtxt(inFile2, dtype=float)
 
 	cla.fit(X, y)
 	cla.save('cla')
 
-	# with open('cla.pkl', 'rb') as f:
-	# 	cla = pickle.load(f)
-	# f.close()
+	H = cla.hlc_sample(800)
+	T = [M for M in np.hsplit(H, 3)]
 
-	# H = cla.hlc_sample(800)
+	with open('a1.txt', 'w') as f:
+		np.savetxt(f, T[0], fmt='%f')
 
-	# with open('hi_disegno_sham_new.txt', 'w') as f:
-	# 	np.savetxt(f, H, fmt='%f')
-	# f.close()
-
-	# cla.plot_accuracy_demo(X_test, y_test)
+	with open('mech.txt', 'w') as f:
+		np.savetxt(f, np.hstack((T[1], T[2])), fmt='%f')
 
 #-------------------------
 
