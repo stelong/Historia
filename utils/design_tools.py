4689d04374fa2c48c31b9f11bd163ffae67623e9
stelong <stefano.longobardi.8@gmail.com>
2019-02-19 11:11:37 +0000

    some new tools

--- utils/design_tools.py
+++ utils/design_tools.py
@@ -20,17 +20,38 @@ def lhd(p0, E, n_samp):
 
 	return H
 
+def split_dataset(X, name_out):
+	T = [M for M in np.vsplit(X, 4)]
+	for i in range(4):
+		with open(name_out + '_' + str(i+1) + '.txt', 'w') as f:
+			np.savetxt(f, T[i], fmt='%f')
+	return
+
 def putlab(X, name_out):
 	n_samp = X.shape[0]
-
+	n_par = X.shape[1]
 	labels = ['-p', '-ap', '-z', '-c1', '-ca50', '-kxb', '-koff', '-Tref']
-	n_par = len(labels)
-
-	with open(name_out, 'w') as f:
+	with open(name_out + '.txt', 'w') as f:
 		for i in range(n_samp):
 			for j in range(n_par):
-				f.write('%s %g ' % (labels[j], X[i, j]))
+				f.write('{} {:g} '.format(labels[j], X[i, j]))
 			f.write('\n')
-	f.close()
-
 	return
+
+def divide_et_impera(name_in):
+	with open(name_in + '.txt', 'r') as f:
+		lines = f.readlines()
+		for i in range(4):
+			with open(name_in + '_' + str(i+1) + '.txt', 'w') as fi:
+				for c, line in enumerate(lines):
+					if c // 200 == i:
+						fi.write(line)
+
+def append_to_header(rat, ca_traces, number):
+	with open('utils/headers/' + rat + '/MeshFlatBase.in', 'r') as f:
+		with open('MeshFlatBase_' + str(number) + '.in', 'w') as fh:
+			for line in f:
+				fh.write(line)
+			with open(ca_traces + '_' + str(number) + '.txt', 'r') as fc:
+				for line in fc:
+					fh.write(line)
