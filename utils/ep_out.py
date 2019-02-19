0ac975f94e55144ab32b51a739ec7eb441800622
stelong <stefano.longobardi.8@gmail.com>
2019-02-19 11:12:07 +0000

    improved a lot

--- utils/ep_out.py
+++ utils/ep_out.py
@@ -16,82 +16,85 @@ class PhenCalcium:
 		a0 = A_output(self.ca)
 		a0[2] = np.log(2)/a0[2]
 
-		self.a1 = curve_fit(f, shift_for_a_sec(self.t), self.ca, p0=a0)[0]
+		self.a1 = curve_fit(f, self.t[1:], self.ca[1:], p0=a0)[0]
 		self.a = np.copy(self.a1)
+		self.ca = np.insert(f(self.t[1:], *self.a1), 0, self.a1[0]) 
 
-	def get_biomarkers(self):
+	def get_bio(self):
 		DCa = self.a[0]
 		try:
-			TP = brentq(df1, self.t[1], self.t[-1], args=self.a)
+			TP = brentq(df, self.t[1], self.t[-1], args=self.a)
 		except:
 			TP = -1
 		if TP != -1:
 			PCa = f(TP, *self.a)
 		else:
 			PCa = -1
-		try:
-			RT50 = brentq(df2, TP, self.t[-1], args=(self.a, TP)) - TP
-		except:
+		if TP != -1 and PCa != -1:
+			try:
+				RT50 = brentq(fm, TP, self.t[-1], args=(self.a, DCa, PCa)) - TP
+			except:
+				RT50 = -1
+		else:
 			RT50 = -1
-		self.bio = np.array([DCa, PCa, RT50, TP])
+		
+		self.bio = [DCa, PCa, RT50, TP]
 
-		if self.bio[3] == -1 or self.bio[2] == -1:
-			self.conv = 0
-		elif self.bio[0] < 0 or self.bio[1] < 0 or self.bio[2] < 0 or self.bio[3] < 0:
-			self.conv = 0
-		elif self.bio[0] >= self.bio[1]:
-			self.conv = 0
+	def build_ca(self):
+		self.ca = np.insert(f(self.t[1:], *self.a), 0, self.a[0])
 
-	def build_calcium(self):
-		self.ca = f(self.t, *self.a)
+	def check_ca(self):
+		pi = self.ca[0]
+		pm = f(self.bio[2]+self.bio[3], *self.a)
+		dh = (pm - pi)/2
+		if self.bio[2] == -1 or self.bio[0] >= self.bio[1] or self.ca[-1] > pi+dh:
+			self.conv = 0
+		else:
+			self.conv = 1
+
+	def print_ca(self, f, stim):
+		f.write('ca{} 166 1 '.format(stim))
+		np.savetxt(f, self.ca.reshape(1, -1), fmt='%f')
 
 	def plot(self, visbio=False, scene='do_not_show'):
-		plt.plot(self.t, f(self.t, *self.a))
+		plt.plot(self.t, self.ca, zorder=1)
 		if visbio:
-			plt.scatter(self.bio[3], self.bio[1], c='r')
-			plt.scatter(self.bio[2]+self.bio[3], f(self.bio[2]+self.bio[3], *self.a), c='r')
-			plt.axvline(x=self.bio[3], c='r', linestyle='dashed')
-			plt.axvline(x=self.bio[2]+self.bio[3], c='r', linestyle='dashed')
-			plt.axhline(y=self.bio[0], c='r', linestyle='dashed')
-			plt.axhline(y=self.bio[1], c='r', linestyle='dashed')
+			plt.scatter(self.bio[3], self.bio[1], c='r', zorder=3)
+			plt.scatter(self.bio[2]+self.bio[3], f(self.bio[2]+self.bio[3], *self.a), c='r', zorder=3)
+			plt.axvline(x=self.bio[3], c='r', linestyle='dashed', linewidth=0.8, zorder=2)
+			plt.axvline(x=self.bio[2]+self.bio[3], c='r', linestyle='dashed', linewidth=0.8, zorder=2)
+			plt.axhline(y=self.bio[0], c='r', linestyle='dashed', linewidth=0.8, zorder=2)
+			plt.axhline(y=self.bio[1], c='r', linestyle='dashed', linewidth=0.8, zorder=2)
 		if scene == 'show':
 			plt.xlim(self.t[0], self.t[-1])
 			plt.xlabel('Time [ms]')
 			plt.ylabel('Intracellular calcium [$\mu$M]')
 			plt.show()
 
-	def print_calcium(self, f, stim):
-		f.write('ca{} 166 1 '.format(stim))
-		np.savetxt(f, self.ca.reshape(1, -1), fmt='%f')
-
-# ---------------
-
 def A_output(ca):
 	N = len(ca)
 	t = np.linspace(0, N-1, N)
 
-	DCa = np.min(ca)
-	PCa = np.max(ca)
-	i_max = np.argmax(ca)
-	TP = t[i_max]
+	DCa = ca[0]
+	imax = np.argmax(ca)
+	PCa = ca[imax]
+	TP = t[imax]
 
-	for i in range(i_max, N):
-		if ca[i] <= 0.5*(DCa + PCa):
-			RT50 = t[i] - TP
+	j = imax + 1
+	RT50 = -1
+	while j < N:
+		if ca[j] <= 0.5*(PCa + DCa):
+			RT50 = t[j] - TP
 			break
-		else:
-			RT50 = t[-1] - TP
-
+		j += 1
+		
 	return [DCa, PCa, RT50, TP]
 
-def shift_for_a_sec(ts):
-	return [x + 1e-16 for x in ts]
-
 def f(t, *a):
-	return (a[1] - a[0]) * np.power(np.power(t/a[3], -1.0) + np.exp(a[2]*(t - a[3])), -1.0) + a[0]
+	return (a[1] - a[0]) * np.power(a[3]/t + np.exp(a[2]*(t - a[3])), -1.0) + a[0]
 
-def df1(t, a):
-	return np.power(np.power(t/a[3], -1.0) + np.exp(a[2]*(t - a[3])), -2.0) * (-a[3]*np.power(t, -2.0) + a[2]*np.exp(a[2]*(t - a[3])))
+def df(t, a):
+	return a[2]*np.exp(a[2]*(t - a[3])) - a[3]/np.power(t, 2) 
 
-def df2(t, a, b):
-	return f(t, *a) - 0.5*(f(b, *a) + f(0, *a))
+def fm(t, a, pmin, pmax):
+	return f(t, *a) - 0.5*(pmin + pmax)
