9b0ab21067c6ffbe7506cff6e2cd0fc8707506ac
stelong <stefano.longobardi.8@gmail.com>
2019-03-18 14:27:53 +0000

    train a sublist of indices

--- shared/train_utils.py
+++ shared/train_utils.py
@@ -1,5 +1,5 @@
-from history.classifier import svm
-from history.emulator import gp
+from Historia.classifier import svm
+from Historia.emulator import gp
 
 def train_classifier(X, y, name_out):
 	cla = svm.SVMCla()
@@ -7,13 +7,12 @@ def train_classifier(X, y, name_out):
 	cla.save(name_out)
 	return
 
-def train_emulator(X, Y, name_out, feats='all'):
+def train_emulator(X, Y, sublist, name_out, feats='all'):
 	if feats == 'single':
-		out_dim = Y.shape[1]
-		for i in range(out_dim):
+		for i in sublist:
 			emul = gp.GPEmul()
 			emul.fit(X, Y[:, i])
-			emul.save(name_out + '_' + str(i+1))
+			emul.save(name_out + '_' + str(i))
 	else:
 		emul = gp.GPEmul()
 		emul.fit(X, Y)
