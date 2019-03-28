534c6887c18f5447aea0714254c40472bd179e4b
stelong <stefano.longobardi.8@gmail.com>
2019-03-28 10:48:05 +0000

    union + filter

--- shared/indices_utils.py
+++ shared/indices_utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.stats import zscore
 
 def diff(l1, l2):
 	return list(set(l1) - set(l2))
@@ -8,6 +9,9 @@ def inters(l1, l2):
 
 def inters_many(L):
 	return list(set.intersection(*map(lambda x: set(x), L)))
+
+def union_many(L):
+	return list(set.union(*map(lambda x: set(x), L)))
 
 def restrict_kth_comp(data, k, ib, ub):
 	l = []
@@ -28,8 +32,20 @@ def whereq_whernot(M, SM):
 	return l, diff(range(M.shape[0]), l)
 
 
-# def whereq_whernot(M, SM): # PROBABLY A BUG IN NPWHERE
-# 	l = []
-# 	for i in range(SM.shape[0]):
-# 		l.append(np.where(M == SM[i, :])[0][0])
-# 	return l, diff(range(M.shape[0]), l)
+def whereq_whernot_b(M, SM): # PROBABLY A BUG IN NPWHERE
+	l = []
+	for i in range(SM.shape[0]):
+		l.append(np.where(M == SM[i, :])[0][0])
+	return l, diff(range(M.shape[0]), l)
+
+def filter_zscore(X):
+	samp_dim = X.shape[0]
+	feat_dim = X.shape[1]
+	L = []
+	thre = 3.0
+	for j in range(feat_dim):
+		z = np.abs(zscore(X[:, j]))
+		L.append(list(np.where(z > 3)[0]))
+	l = union_many(L)
+	l.sort()
+	return l
