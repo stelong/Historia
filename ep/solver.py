8a8c48713d5d485c221016ed1c6a6eba8a5d6565
stelong <stefano.longobardi.8@gmail.com>
2019-04-26 13:37:58 +0100

    renewed

--- ep/solver.py
+++ ep/solver.py
@@ -2,40 +2,52 @@ import numpy as np
 from scipy.integrate import solve_ivp
 
 class EPSolution:
-	def __init__(self, odesys, model):
-		self.odesys = odesys
+	def __init__(self, model, rat):
+		self.rat = rat
 		self.model = model
-		self.t = np.arange(0, 170+1, 1)
-		self.Y = np.zeros((170+1, 18), dtype=float)
-		self.ca = np.zeros((170+1,), dtype=float)
 
-	def run2sc(self, parameters):
+	def run2sc(self, parameters, nbeats):
 		self.conv = 1
-		tspan = [0, 170]
-		Y0 = self.model.initStates()
+		self.nbeats = nbeats
+		self.t = np.arange(nbeats*170)
+		tspan = [0, nbeats*170-1]
+		Y0 = self.rat.initStates()
 		c = self.model.initConsts(parameters)
-		Y = solve_ivp(fun=lambda t, y: self.odesys.f(t, y, c), t_span=tspan, y0=Y0, method='BDF', t_eval=self.t, max_step=1.0)
-		self.Y = Y.y
-		self.ca = 1e3*Y.y[12, :]
-
+		try:
+			Y = solve_ivp(fun=lambda t, y: self.model.computeRates(t, y, c), t_span=tspan, y0=Y0, method='BDF', t_eval=self.t, max_step=1.0)
+			self.Y = Y.y
+			self.ca = 1e3*Y.y[12, :]
+		except:
+			print('=== ODE solver failed!')
+			self.conv = 0
+			self.Y = []
+			self.ca = []
+			
 	def run2ss(self, parameters):
 		self.conv = 1
-		tspan = [0, 170]
-		Y0 = self.model.initStates()
+		self.t = np.arange(170)
+		tspan = [0, 170-1]
+		Y0 = self.rat.initStates()
 		c = self.model.initConsts(parameters)
-		Y = solve_ivp(fun=lambda t, y: self.odesys.f(t, y, c), t_span=tspan, y0=Y0, method='BDF', t_eval=self.t)
+		try:
+			Y = solve_ivp(fun=lambda t, y: self.model.computeRates(t, y, c), t_span=tspan, y0=Y0, method='BDF', t_eval=self.t)
 
-		thre = 100
-		ssnc = 1
-		while np.linalg.norm((Y.y[:, -1] - Y.y[:, 0])/Y.y[:, 0], ord=np.inf) > 1e-3:
-			Y = solve_ivp(fun=lambda t, y: self.odesys.f(t, y, c), t_span=tspan, y0=Y.y[:, -1], method='BDF', t_eval=self.t)
-			ssnc += 1
-			if ssnc == thre:
-				self.conv = 0
-				break
-		if self.conv:
-			print('\n=== Number of cycles needed to reach the steady-state: {}\n'.format(ssnc))
-			self.Y = Y.y
-			self.ca = 1e3*Y.y[12, :]			
-		else:
-			print('\n=== Error: unable to reach the steady-state!\n')
+			thre = 100
+			ssnc = 1
+			while np.linalg.norm((Y.y[:, -1] - Y.y[:, 0])/Y.y[:, 0], ord=np.inf) > 1e-3:
+				Y = solve_ivp(fun=lambda t, y: self.model.computeRates(t, y, c), t_span=tspan, y0=Y.y[:, -1], method='BDF', t_eval=self.t)
+				ssnc += 1
+				if ssnc == thre:
+					print('\n=== Error: unable to reach the steady-state!\n')
+					self.conv = 0
+					self.Y = []
+					self.ca = []
+					break
+
+			if self.conv:
+				print('\n=== Number of cycles needed to reach the steady-state: {}\n'.format(ssnc))
+				self.Y = Y.y
+				self.ca = 1e3*Y.y[12, :]
+		except:
+			print('=== ODE solver failed!')
+			self.conv = 0
