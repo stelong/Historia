aa35f36ea99c7f30af3bfacbc36a6031092ade0e
stelong <stefano.longobardi.8@gmail.com>
2019-04-27 16:47:23 +0100

    material design colors

--- shared/plot_utils.py
+++ shared/plot_utils.py
@@ -4,13 +4,37 @@ import numpy as np
 import seaborn as sns
 from sklearn import metrics
 
+def get_col(color_name):
+	colors = {
+		'red' : [[255, 205, 210], [183, 28, 28]],
+		'pink' : [[248, 187, 208], [136, 14, 79]],
+		'purple' : [[225, 190, 231], [74, 20, 140]],
+		'deep_purple' : [[209, 196, 233], [49, 27, 146]],
+		'indigo' : [[197, 202, 233], [26, 35, 126]],
+		'blue' : [[187, 222, 251], [13, 71, 161]],
+		'light_blue' : [[179, 229, 252], [1, 87, 155]],
+		'cyan' : [[178, 235, 242], [0, 96, 100]],
+		'teal' : [[178, 223, 219], [0, 77, 64]],
+		'green' : [[200, 230, 201], [27, 94, 32]],
+		'light_green' : [[220, 237, 200], [51, 105, 30]],
+		'lime' : [[240, 244, 195], [130, 119, 23]],
+		'yellow' : [[255, 249, 196], [245, 127, 23]],
+		'amber' : [[255, 236, 179], [255, 111, 0]],
+		'orange' : [[255, 224, 178], [230, 81, 0]],
+		'deep_orange' : [[255, 204, 188], [191, 54, 12]],
+		'brown' : [[215, 204, 200], [62, 39, 35]],
+		'gray' : [[245, 245, 245], [33, 33, 33]],
+		'blue_gray' : [[207, 216, 220], [38, 50, 56]]
+	}
+	return colors[color_name]
+
 def interp_col(color, n):
 	"""Linearly interpolate a color.
 	Inputs:
 		- color: list with two elements:
 			color[0] = lightest color variant (RGB-triplet in [0, 255])
 			color[1] = darkest color variant (RGB-triplet in [0, 255])
-		- n: number of desired output colors (n > 2).
+		- n: number of desired output colors (n >= 2).
 	Output:
 		- lsc: list of n linearly scaled colors.
 	"""
