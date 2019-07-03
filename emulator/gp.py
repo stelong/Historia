2d6c3c696105f963664d196af0241b0c000031bd
stelong <stefano.longobardi.8@gmail.com>
2019-07-03 16:54:28 +0100

    now explained

--- emulator/gp.py
+++ emulator/gp.py
@@ -8,6 +8,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
 class GPEmul:
+	"""This class implements a scikit-learn instance of a Gaussian Process (GP) regressor.
+	Firstly, a linear regression model (counting up to 3rd order interactions) is used to predict the mean of the data m(X). Then, a GP is trained on the residuals Y - m(X).
+	The GP's 'kernel' component is automatically chosen to be the best scoring in a five-fold cross-validation test among all the possible kernel choices from a given grid.
+	"""
 	def __init__(self):
 		self.X = []
 		self.Y = []
@@ -15,6 +19,11 @@ class GPEmul:
 		self.gp = []
 
 	def fit(self, X, Y):
+		"""Fit a GP regressor on the training set (X, Y).
+		Args:
+			- X: (n, m1)-shaped matrix
+			- Y: (n, m2)-shaped matrix.
+		"""
 		self.X = X
 		self.Y = Y
 
@@ -40,6 +49,16 @@ class GPEmul:
 		self.gp = gs2.best_estimator_
 
 	def predict(self, X_new, with_std=True):
+		"""Predict the output of the mapping F:X->Y for each input point in a new set.
+		Arg:
+			X_new: (n, m1)-shaped matrix.
+		Kwarg:
+			with_std: boolean, defining whether to output also the std or not.
+		Output:
+			- (n, m2)-shaped matrix of GP mean values, if with_std=False
+			- (n,)-, (n,)-shaped vectors of respectively GP mean and GP std values, if with_std=True.
+			NOTE: for the 'with_std=True' case, the GP must be trained on a single-component output, e.g. (X, Y[:, i]).
+		"""
 		if not with_std:
 			return self.mean.predict(X_new) + self.gp.predict(X_new)
 		else: 
@@ -47,10 +66,18 @@ class GPEmul:
 			return self.mean.predict(X_new) + res, std
 
 	def save(self, name):
+		"""Save the emulator into a binary file.
+		Arg:
+			name: string representing the output file name.
+		"""
 		with open(name + '.pkl', 'wb') as f:
 			pickle.dump(self, f)
 
 	def load(self, name):
+		"""Load the emulator from a binary file.
+		Arg:
+			name: string representing the input file name.
+		"""
 		with open(name + '.pkl', 'rb') as f:
 			vars = pickle.load(f)
 		self.X = vars.X
