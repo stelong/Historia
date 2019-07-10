0b899794b3ed1f5d333388249a862972ca8de7a6
stelong <stefano.longobardi.8@gmail.com>
2019-07-10 16:33:51 +0100

    hardcoded L matrix

--- shared/mesh_utils.py
+++ shared/mesh_utils.py
@@ -13,10 +13,75 @@ def l(i, x):
 	return num/den
 
 def psi(i, x):
-	ind = [0, 1, 2, 3]
-	L = [p for p in itertools.product(ind, repeat=3)][i]
+	# ind = [0, 1, 2, 3]
+	# L = [p for p in itertools.product(ind, repeat=3)][i]
 
-	return l(L[0], x[0])*l(L[1], x[1])*l(L[2], x[2])
+	L = [[0, 0, 0],
+		[1, 0, 0],
+		[2, 0, 0],
+		[3, 0, 0],
+		[0, 1, 0],
+		[1, 1, 0],
+		[2, 1, 0],
+		[3, 1, 0],
+		[0, 2, 0],
+		[1, 2, 0],
+		[2, 2, 0],
+		[3, 2, 0],
+		[0, 3, 0],
+		[1, 3, 0],
+		[2, 3, 0],
+		[3, 3, 0],
+		[0, 0, 1],
+		[1, 0, 1],
+		[2, 0, 1],
+		[3, 0, 1],
+		[0, 1, 1],
+		[1, 1, 1],
+		[2, 1, 1],
+		[3, 1, 1],
+		[0, 2, 1],
+		[1, 2, 1],
+		[2, 2, 1],
+		[3, 2, 1],
+		[0, 3, 1],
+		[1, 3, 1],
+		[2, 3, 1],
+		[3, 3, 1],
+		[0, 0, 2],
+		[1, 0, 2],
+		[2, 0, 2],
+		[3, 0, 2],
+		[0, 1, 2],
+		[1, 1, 2],
+		[2, 1, 2],
+		[3, 1, 2],
+		[0, 2, 2],
+		[1, 2, 2],
+		[2, 2, 2],
+		[3, 2, 2],
+		[0, 3, 2],
+		[1, 3, 2],
+		[2, 3, 2],
+		[3, 3, 2],
+		[0, 0, 3],
+		[1, 0, 3],
+		[2, 0, 3],
+		[3, 0, 3],
+		[0, 1, 3],
+		[1, 1, 3],
+		[2, 1, 3],
+		[3, 1, 3],
+		[0, 2, 3],
+		[1, 2, 3],
+		[2, 2, 3],
+		[3, 2, 3],
+		[0, 3, 3],
+		[1, 3, 3],
+		[2, 3, 3],
+		[3, 3, 3]]
+
+	return l(L[i][0], x[0])*l(L[i][1], x[1])*l(L[i][2], x[2])
 
 def PSI(c, x):
 	val = 0
@@ -55,7 +120,7 @@ def exelem(file):
 
 def exdata_xi(file):
 	C = []
-	Mt = []
+	Ft = []
 	JX = []
 	with open(file, 'r') as f:
 		for _ in range(17):
@@ -64,29 +129,31 @@ def exdata_xi(file):
 			if (i-5) % 4 == 0:
 				C.append( [float(line.split()[j]) for j in range(3, 6)] )
 			elif (i-6) % 4 == 0:
-				Mt.append( [float(line.split()[j]) for j in range(9)] )
+				Ft.append( [float(line.split()[j]) for j in range(3)] )
 			elif (i-7) % 4 == 0:
 				JX.append( float(line) )
 	C = np.array(C)
-	Mt = np.array(Mt)
-	M = [m for m in np.vsplit(Mt, 108)]
+	Ft = np.array(Ft)
+	F = [f for f in np.vsplit(Ft, 108)]
 	JX = np.array(JX)
 
-	return C, M, JX
+	return C, F, JX
 
 def exdata_ta_lambda(file):
-	Ta = []
-	lambd = []
+	Tat = []
+	lambdt = []
 	with open(file, 'r') as f:
 		for _ in range(22):
 			next(f)
 		for i, line in enumerate(f):
 			if (i-4) % 3 == 0:
 				v = [float(line.split()[j]) for j in range(2, 4)]
-				Ta.append( v[0] )
-				lambd.append( v[1] )
-	Ta = np.array(Ta)
-	lambd = np.array(lambd)
+				Tat.append( v[0] )
+				lambdt.append( v[1] )
+	Tat = np.array(Tat).reshape(-1, 1)
+	lambdt = np.array(lambdt).reshape(-1, 1)
+	Ta = [t for t in np.vsplit(Tat, 108)]
+	lambd = [l for l in np.vsplit(lambdt, 108)]
 
 	return Ta, lambd
 
@@ -100,42 +167,42 @@ def connect(B, n_nodes, n_elements):
 		L.append(l)
 	return L
 
-def Tmap(A, B, C, M, n_elements):
-	r = np.linspace(0, 1, 4)
-	X, Y, Z = np.meshgrid(r, r, r)
-	R = np.hstack(( np.hstack(( X.reshape(64, 1), Y.reshape(64, 1) )), Z.reshape(64, 1) ))
+# def Tmap(A, B, C, M, n_elements):
+# 	r = np.linspace(0, 1, 4)
+# 	X, Y, Z = np.meshgrid(r, r, r)
+# 	R = np.hstack(( np.hstack(( X.reshape(64, 1), Y.reshape(64, 1) )), Z.reshape(64, 1) ))
 
-	coeff = [70, -140, 90, -20, 1]
+# 	coeff = [70, -140, 90, -20, 1]
 
-	rg = np.roots(coeff)
-	Xg, Yg, Zg = np.meshgrid(rg, rg, rg)
-	Rg = np.hstack(( np.hstack(( Xg.reshape(64, 1), Yg.reshape(64, 1) )), Zg.reshape(64, 1) ))
+# 	rg = np.roots(coeff)
+# 	Xg, Yg, Zg = np.meshgrid(rg, rg, rg)
+# 	Rg = np.hstack(( np.hstack(( Xg.reshape(64, 1), Yg.reshape(64, 1) )), Zg.reshape(64, 1) ))
 
-	elem = 80
+# 	elem = 80
 
-	W = []
-	Wg = []
-	Z = []
-	CC = C[:64]
-	for j in range(elem, elem+1):
-		w = np.zeros((64, 3), dtype=float)
-		wg = np.zeros((64, 3), dtype=float)
-		z = np.zeros((64, 3), dtype=float)
-		for i in range(64):
-			w[i, :] = T(A[list(B[j, :]-1), :], R[i, :]).reshape(1, -1)
-			wg[i, :] = T(A[list(B[j, :]-1), :], CC[i, :]).reshape(1, -1)
-			z[i, :] = T(A[list(B[j, :]-1), :], CC[i, :] + M[j][i, :3]).reshape(1, -1)
-		W.append(w)
-		Wg.append(wg)
-		Z.append(z)
+# 	W = []
+# 	Wg = []
+# 	Z = []
+# 	CC = C[:64]
+# 	for j in range(elem, elem+1):
+# 		w = np.zeros((64, 3), dtype=float)
+# 		wg = np.zeros((64, 3), dtype=float)
+# 		z = np.zeros((64, 3), dtype=float)
+# 		for i in range(64):
+# 			w[i, :] = T(A[list(B[j, :]-1), :], R[i, :]).reshape(1, -1)
+# 			wg[i, :] = T(A[list(B[j, :]-1), :], CC[i, :]).reshape(1, -1)
+# 			z[i, :] = T(A[list(B[j, :]-1), :], CC[i, :] + M[j][i, :3]).reshape(1, -1)
+# 		W.append(w)
+# 		Wg.append(wg)
+# 		Z.append(z)
 
-	fig = plt.figure()
-	ax = fig.add_subplot(111, projection='3d')
-	ax.scatter(W[0][:, 0], W[0][:, 1], W[0][:, 2], c='b')
-	ax.scatter(Wg[0][:, 0], Wg[0][:, 1], Wg[0][:, 2], c='r')
-	for i in range(64):
-		ax.plot([Wg[0][i, 0],Z[0][i, 0]], [Wg[0][i, 1],Z[0][i, 1]], [Wg[0][i, 2],Z[0][i, 2]], c='r')
-	plt.show()
+# 	fig = plt.figure()
+# 	ax = fig.add_subplot(111, projection='3d')
+# 	ax.scatter(W[0][:, 0], W[0][:, 1], W[0][:, 2], c='b')
+# 	ax.scatter(Wg[0][:, 0], Wg[0][:, 1], Wg[0][:, 2], c='r')
+# 	for i in range(64):
+# 		ax.plot([Wg[0][i, 0],Z[0][i, 0]], [Wg[0][i, 1],Z[0][i, 1]], [Wg[0][i, 2],Z[0][i, 2]], c='r')
+# 	plt.show()
 
 
 
