8e6be3ea25b3757bc4224e226a5631493ebade45
stelong <stefano.longobardi.8@gmail.com>
2019-11-19 16:15:09 +0000

    added new utility fun

--- emulator/gp.py
+++ emulator/gp.py
@@ -8,9 +8,9 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import PolynomialFeatures
 
 class GPEmul:
-	"""This class implements a scikit-learn instance of a Gaussian Process (GP) regressor.
-	Firstly, a linear regression model (counting up to 3rd order interactions) is used to predict the mean of the data m(X). Then, a GP is trained on the residuals Y - m(X).
-	The GP's 'kernel' component is automatically chosen to be the best scoring in a five-fold cross-validation test among all the possible kernel choices from a given grid.
+	"""This class wraps the scikit-learn Gaussian Process (GP) regressor to build a GP-based emulator.
+	Firstly, a linear regression model (counting up to 3rd order interactions) is used to predict the mean of the data m(X). Then, a zero-mean GP is trained on the residuals Y - m(X).
+	The emulator's mean function and GP's 'kernel' component are automatically chosen to be the best scoring in a five-fold cross-validation test among all the possible kernel choices from a given grid.
 	"""
 	def __init__(self):
 		self.X = []
@@ -47,8 +47,9 @@ class GPEmul:
 			n_jobs=-1, iid=False, cv=5, return_train_score=False)
 		gs2.fit(self.X, residuals)
 		self.gp = gs2.best_estimator_
+		return
 
-	def predict(self, X_new, with_std=True):
+	def predict(self, X_new):
 		"""Predict the output of the mapping F:X->Y for each input point in a new set.
 		Arg:
 			X_new: (n, m1)-shaped matrix.
@@ -59,19 +60,17 @@ class GPEmul:
 			- (n,)-, (n,)-shaped vectors of respectively GP mean and GP std values, if with_std=True.
 			NOTE: for the 'with_std=True' case, the GP must be trained on a single-component output, e.g. (X, Y[:, i]).
 		"""
-		if not with_std:
-			return self.mean.predict(X_new) + self.gp.predict(X_new)
-		else: 
-			res, std = self.gp.predict(X_new, return_std=True)
-			return self.mean.predict(X_new) + res, std
+		res, std = self.gp.predict(X_new, return_std=True)
+		return self.mean.predict(X_new) + res, std
 
 	def save(self, name):
 		"""Save the emulator into a binary file.
 		Arg:
 			name: string representing the output file name.
 		"""
-		with open(name + '.pkl', 'wb') as f:
+		with open(name + '.pickle', 'wb') as f:
 			pickle.dump(self, f)
+		return
 
 	@classmethod
 	def load(cls, name):
@@ -79,6 +78,44 @@ class GPEmul:
 		Arg:
 			name: string representing the input file name.
 		"""
-		with open(name + '.pkl', 'rb') as f:
+		with open(name + '.pickle', 'rb') as f:
 			emul_obj = pickle.load(f)
 		return emul_obj
+
+
+class GPEmulUtils:
+	"""Utility class to extend GPEmul class functionality.
+	"""
+
+	@classmethod
+	def train(cls, X, Y, name, active_out_feats=None):
+		"""Train GP emulator/s.
+		Args:
+			- X: (n, m1)-shaped matrix
+			- Y: (n, m2)-shaped matrix
+			- active_out_feats = list of indices representing the output features to fit
+			- name: string representing the prefix of the output file's/s' name/s.
+		"""
+		if active_out_feats is not None:
+			for i in active_out_feats:
+				emul = GPEmul()
+				emul.fit(X, Y[:, i])
+				emul.save(name + '_' + str(i))
+		else:
+			emul = GPEmul()
+			emul.fit(X, Y)
+			emul.save(name)
+		return
+
+	@classmethod
+	"""Load GP emulator/s into a list.
+		Args:
+			- name: string representing the prefix of the output file's/s' name/s.
+			- active_out_feats = list of indices representing the output features to fit
+	"""
+	def load_emulators(cls, name, active_out_feats):
+		emulator = []
+		for i in active_out_feats:
+			emul = GPEmul.load(name + '_' + str(i))
+			emulator.append(emul)
+		return emulator
