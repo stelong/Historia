f37eda1d59870b3a93ee854d3a598e352be6f695
stelong <stefano.longobardi.8@gmail.com>
2020-02-24 16:56:32 +0000

    create .in file differently

--- shared/ep_utils.py
+++ shared/ep_utils.py
@@ -8,12 +8,12 @@ def divide_et_impera(name_in):
 						fi.write(line)
 	return
 
-def append_to_header(rat, ca_traces, number):
-	with open('utils/headers/' + rat + '/MeshFlatBase.in', 'r') as f:
-		with open('MeshFlatBase_' + str(number) + '.in', 'w') as fh:
+def init_header(rat, n_traces, filename):
+	with open('Historia/shared/headers/' + rat + '/MeshFlatBase.in', 'r') as f:
+		with open(filename + '.in', 'w') as fh:
 			for line in f:
 				fh.write(line)
-			with open(ca_traces + '_' + str(number) + '.txt', 'r') as fc:
-				for line in fc:
-					fh.write(line)
+			fh.write('\n')
+			fh.write('stims: {}'.format(n_traces))
+			fh.write('\n')
 	return
