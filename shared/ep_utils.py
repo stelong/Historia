4380d982c2fa70a725c5380c393a954ea5c2444e
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2020-03-29 10:11:07 +0100

    cleaner version

--- shared/ep_utils.py
+++ shared/ep_utils.py
@@ -1,19 +1,16 @@
-def divide_et_impera(name_in):
-	with open(name_in + '.txt', 'r') as f:
-		lines = f.readlines()
-		for i in range(4):
-			with open(name_in + '_' + str(i+1) + '.txt', 'w') as fi:
-				for c, line in enumerate(lines):
-					if c // 200 == i:
-						fi.write(line)
-	return
+import numpy as np
 
-def init_header(rat, n_traces, filename):
+def init_header(rat, n_stims, filename):
 	with open('Historia/shared/headers/' + rat + '/MeshFlatBase.in', 'r') as f:
 		with open(filename + '.in', 'w') as fh:
 			for line in f:
 				fh.write(line)
 			fh.write('\n')
-			fh.write('stims: {}'.format(n_traces))
+			fh.write('stims: {}'.format(n_stims))
 			fh.write('\n')
+	return
+
+def append_to_header(f, stim, ca):
+	f.write('ca{} 166 1 '.format(stim))
+	np.savetxt(f, ca.reshape(1, -1), fmt='%.6f')
 	return
