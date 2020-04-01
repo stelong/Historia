d67d821287cf84650c47794245574fcc6b213071
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2020-04-01 16:42:36 +0100

    gonna come back to this

--- ep/solver.py
+++ ep/solver.py
@@ -3,8 +3,8 @@ import numpy as np
 from scipy.integrate import solve_ivp
 
 class EPSolution:
-	"""This class implements the solution of Gattoni's model* ODEs system.
-	*https://physoc.onlinelibrary.wiley.com/doi/10.1113/JP273879
+	"""This class implements the solution of Gattoni et al. (2016) electrophysiology model*.
+	*https://physoc.onlinelibrary.wiley.com/doi/full/10.1113/JP273879
 	"""
 	def __init__(self, model, rat, hz):
 		self.model = model
@@ -15,14 +15,14 @@ class EPSolution:
 		"""Run the ODE solver and store the solution at 1ms spaced time points.
 		Args:
 			- parameters: (n,)-shaped vector to be initialized through the dedicated module 'initialize.py'
-			- nbeats: strictly positive integer, representing the number of beats we want to simulate.
+			- nbeats: strictly positive integer, representing the number of beats to simulate.
 		"""
 		self.conv = 1
 		self.nbeats = nbeats
 
 		stim_period = parameters[16]
-		self.t = np.arange(nbeats*stim_period+1)
-		tspan = [0.0, nbeats*stim_period]
+		self.t = np.arange(nbeats*stim_period + 1)
+		tspan = [0, nbeats*stim_period]
 
 		Y0 = init.states(self.rat, self.hz)
 		c = self.model.initConsts(parameters)
