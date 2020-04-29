8b590d43b7a3cb4f6d67ca84f4247a85329c8f09
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2020-04-29 17:12:00 +0100

    easy serialize objects with pickle

---
+++ shared/io_utils.py
@@ -0,0 +1,11 @@
+import pickle
+
+def save(obj, filename):
+	with open(filename + '.pickle', 'wb') as f:
+		pickle.dump(obj, f, protocol=pickle.HIGHEST_PROTOCOL)
+	return
+
+def load(filename):
+	with open(filename + '.pickle', 'rb') as f:
+		obj = pickle.load(f)
+	return obj
