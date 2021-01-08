5d35b5d7f021da2801e1e6aefa7ae3a9a35e21e3
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-01-08 13:01:40 +0100

    new default

--- cellcontr/model/Land2012.py
+++ cellcontr/model/Land2012.py
@@ -1,79 +1,87 @@
 import numpy as np
 
+
 def initStates():
 	sizeStates = 4
-	state = np.zeros((sizeStates,), dtype=float)
+	states = np.zeros((sizeStates,), dtype=float)
 
-	state[0] = 8e-02
-	state[1] = 2e-03
-	state[2] = 0.0
-	state[3] = 0.0
+	states[0] = 8e-02
+	states[1] = 2e-03
+	states[2] = 0.0
+	states[3] = 0.0
 	
-	return state
+	return states
 
-def initConstants(c_dict):
+
+def initConsts(c_dict):
 	sizeConstants = 17
-	constant = np.zeros((sizeConstants,), dtype=float)
+	constants = np.zeros((sizeConstants,), dtype=float)
 
 	for i, key in enumerate(c_dict.keys()):
-		constant[i] = c_dict[key]
+		constants[i] = c_dict[key]
 
-	return constant
+	return constants
 
-def computeAlgebraics(t, y, Cai, constant):
+
+def computeAlgebraics(t, y, Cai, constants):
 	sizeAlgebraics = 10
-	algebraic = np.zeros((sizeAlgebraics,), dtype=float)
+	algebraics = np.zeros((sizeAlgebraics,), dtype=float)
 
-	algebraic[0] = t - len(Cai)*np.floor(t/len(Cai))
+	algebraics[0] = t - len(Cai)*np.floor(t/len(Cai))
 	
 	if not t // len(Cai):
-		algebraic[1] = Cai[int(algebraic[0])]
+		algebraics[1] = Cai[int(algebraics[0])]
 	else:
-		algebraic[1] = Cai[-1]
+		algebraics[1] = Cai[-1]
 
-	algebraic[2] = constant[2]*(1.0 + constant[11]*(np.min([1.2, np.max([0.8, constant[0]])]) - 1.0))
-	algebraic[3] = np.sqrt(np.power(y[0]/constant[4], constant[8]))
-	algebraic[4] = np.min([100.0, 1.0/algebraic[3]])
-	algebraic[5] = y[2] + y[3]
+	algebraics[2] = constants[2]*(1.0 + constants[11]*(constants[0] - 1.0)) # safe lambda = np.min([1.2, np.max([0.8, constants[0]])])
+	algebraics[3] = np.sqrt(np.power(y[0]/constants[4], constants[8]))
+	algebraics[4] = 1.0/algebraics[3] # safe 1/permtot = np.min([100.0, 1.0/algebraics[3]])
+	algebraics[5] = y[2] + y[3]
 
-	if algebraic[5] <= 0.0:
-		algebraic[6] = (constant[12]*algebraic[5] + 1.0)/(1.0 - algebraic[5])
+	if algebraics[5] <= 0.0:
+		algebraics[6] = (constants[12]*algebraics[5] + 1.0)/(1.0 - algebraics[5])
 	else:
-		algebraic[6] = (1.0 + (constant[12] + 2.0)*algebraic[5])/(1.0 + algebraic[5])
+		algebraics[6] = (1.0 + (constants[12] + 2.0)*algebraics[5])/(1.0 + algebraics[5])
 
-	algebraic[7] = np.max([0.0, 1.0 + constant[10]*(np.min([constant[0], 1.2]) + np.min([np.min([constant[0], 1.2]), 0.87]) - 1.87)])
-	algebraic[8] = algebraic[6] * algebraic[7] * y[1]
-	algebraic[9] = constant[3] * algebraic[8]
+	algebraics[7] = np.max([0.0, 1.0 + constants[10]*(np.min([constants[0], 1.2]) + np.min([np.min([constants[0], 1.2]), 0.87]) - 1.87)])
+	algebraics[8] = algebraics[6] * algebraics[7] * y[1]
+	algebraics[9] = constants[3] * algebraics[8]
 
-	return algebraic
+	return algebraics
+
 
 def computeRates(t, y, Cai, c_dict):
-	constant = initConstants(c_dict)
-	algebraic = computeAlgebraics(t, y, Cai, constant)
+	constants = initConsts(c_dict)
+	algebraics = computeAlgebraics(t, y, Cai, constants)
 
 	sizeRates = 4
-	rate = np.zeros((sizeRates,), dtype=float)
+	rates = np.zeros((sizeRates,), dtype=float)
 
-	rate[0] = constant[6]*np.power(algebraic[1]/algebraic[2], constant[5])*(1.0 - y[0]) - constant[7]*y[0]
-	rate[1] = constant[9]*(algebraic[3]*(1.0 - y[1]) - algebraic[4]*y[1])
-	rate[2] = constant[13]*constant[1] - constant[15]*y[2]
-	rate[3] = constant[14]*constant[1] - constant[16]*y[3]
+	rates[0] = constants[6]*np.power(algebraics[1]/algebraics[2], constants[5])*(1.0 - y[0]) - constants[7]*y[0]
+	rates[1] = constants[9]*(algebraics[3]*(1.0 - y[1]) - algebraics[4]*y[1])
+	rates[2] = constants[13]*constants[1] - constants[15]*y[2]
+	rates[3] = constants[14]*constants[1] - constants[16]*y[3]
 
-	return rate
+	return rates
+
 
 def computeSteadystate(Cai, c_dict):
-	constant = initConstants(c_dict)
-	constant[2] = constant[2]*(1.0 + constant[11]*(np.min([1.2, np.max([0.8, constant[0]])]) - 1.0))
+	constants = initConsts(c_dict)
+	constants[2] = constants[2]*(1.0 + constants[11]*(constants[0] - 1.0))
 
-	r = np.power(Cai/constant[2], constant[5])
-	TRPN_ss = r/(constant[7]/constant[6] + r)
-	trpn_EC50 = np.power(constant[7]/constant[6], 1.0/constant[5])*constant[2]
+	r = np.power(Cai/constants[2], constants[5])
+	TRPN_ss = r/(constants[7]/constants[6] + r)
+	trpn_EC50 = np.power(constants[7]/constants[6], 1.0/constants[5])*constants[2]
 
-	s = np.power(TRPN_ss/constant[4], constant[8])
+	s = np.power(TRPN_ss/constants[4], constants[8])
 	XB_ss = s/(1.0 + s)
-	xb_EC50 = constant[2]*np.power((constant[7]*constant[4])/(constant[6]*(1.0 - constant[4])), 1.0/constant[5])
+	xb_EC50 = constants[2]*np.power((constants[7]*constants[4])/(constants[6]*(1.0 - constants[4])), 1.0/constants[5])
 
-	F_ss = constant[3]*XB_ss
-	pCa50 = -np.log10(1e-6*constant[2]*np.power((constant[7]*constant[4])/(constant[6]*(1.0 - constant[4])), 1.0/constant[5]))
+	hl = np.max([0.0, 1.0 + constants[10]*(np.min([constants[0], 1.2]) + np.min([np.min([constants[0], 1.2]), 0.87]) - 1.87)])
 
-	return TRPN_ss, trpn_EC50, XB_ss, xb_EC50, F_ss, pCa50
+	F_ss = constants[3]*hl*XB_ss
+	pCa50 = -np.log10(1e-6*constants[2]*np.power((constants[7]*constants[4])/(constants[6]*(1.0 - constants[4])), 1.0/constants[5]))
+	nH = constants[8]*constants[5]*(1.0 - constants[4])
+
+	return TRPN_ss, trpn_EC50, XB_ss, xb_EC50, F_ss, pCa50, nH, hl
