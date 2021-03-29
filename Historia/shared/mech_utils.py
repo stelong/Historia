35f93c1a73267d2b912e1f488813620acbd3b8ba
Stefano Longobardi <sl18@bioeng306-lap.isd.kcl.ac.uk>
2021-03-29 15:30:13 +0200

    changed to accomodate more params and out features

--- Historia/shared/mech_utils.py
+++ Historia/shared/mech_utils.py
@@ -5,10 +5,10 @@ from Historia.mech import scan_logfile as slf
 
 
 def extract_features(path_in, dim, path_out, nc):
-    XA = np.zeros((0, 9), dtype=float)
-    X = np.zeros((0, 9), dtype=float)
+    XA = np.zeros((0, 12), dtype=float)
+    X = np.zeros((0, 12), dtype=float)
     YA = []
-    Y = np.zeros((0, 12), dtype=float)
+    Y = np.zeros((0, 14), dtype=float)
     l = []
     for i in range(dim):
         tag = path_in + str(i + 1) + "/output_log.txt"
